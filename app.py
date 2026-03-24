ef89e7659fc31f0da62f0af1c24b388b6a923d93
Chethan R <chethanr7204@gmail.com>
2026-03-24 21:21:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,5 +23,7 @@ def submit():
 
     return "Data Saved Successfully!"
 
-if __name__ == '__main__':
-    app.run(debug=True)
+import os
+
+if _name_ == "_main_":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
